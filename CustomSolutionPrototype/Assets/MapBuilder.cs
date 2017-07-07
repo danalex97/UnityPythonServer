8c8d246bdf97aa3c6328544ed4224b2170bae957
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-07-07 11:52:02 +0100

    Start and Update protocols separated.

--- CustomSolutionPrototype/Assets/MapBuilder.cs
+++ CustomSolutionPrototype/Assets/MapBuilder.cs
@@ -7,49 +7,5 @@ using SimpleJSON;
 
 public class MapBuilder : MonoBehaviour 
 {
-	// Request map dimensions and static objects, i.e. walls.
-	IEnumerator GetFixedObjects()
-	{
-		UnityWebRequest request = UnityWebRequest.Get("http://127.0.0.1:4000/start");
-		yield return request.Send();
 
-		var map = JSON.Parse (request.downloadHandler.text);
-
-		// Create plane (floor).
-		float minX = (float) map["minX"].AsInt;
-		float minY = (float) map["minY"].AsInt;
-		float maxX = (float) map["maxX"].AsInt;
-		float maxY = (float) map["maxY"].AsInt;
-
-		GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
-		floor.transform.position = new Vector3(minX + maxX, 0.0f, minY + maxY);
-		floor.transform.localScale = new Vector3((maxX - minX) / 10.0f, 1.0f, (maxY - minY) / 10.0f);
-
-		// Create cubes (walls).
-		var objectList = map["objects"];
-
-		for (int i = 0; i < objectList.Count; i++)
-		{
-			var obj = objectList[i];
-
-			string id = "object" + Convert.ToString(obj["id"].AsInt);
-
-			float x = (float) obj["x"].AsInt;
-			float y = (float) obj["y"].AsInt;
-
-			// Create new game object.
-			GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			wall.transform.position = new Vector3(x, 0.5f, y);
-			wall.name = id;
-
-			Debug.Log("Object " + id + " is at position (" + x + ", " + y + ")");
-		}
-	}
-
-	// Wrap initialisation request in coroutine.
-	void BuildStaticScene()
-	{
-		Debug.Log("Initialising map...");
-		StartCoroutine(GetFixedObjects());
-	}
 }
--- CustomSolutionPrototype/Assets/WorkerManager.cs
+++ CustomSolutionPrototype/Assets/WorkerManager.cs
@@ -7,16 +7,12 @@ using SimpleJSON;
 
 public class WorkerManager : MonoBehaviour
 {
-	private const string reqeustUrl = "http://127.0.0.1:4000";
-
 	// Setup to ping the server every second.
 	void Start()
 	{
 		Debug.Log("Starting the game...");
 
-		MapBuilder mapBuilder = new MapBuilder();
-		mapBuilder.
-
+		BuildStaticScene();
 		InvokeRepeating("UpdateMapState", 0.0f, 1.0f);
 	}
 		
@@ -31,7 +27,7 @@ public class WorkerManager : MonoBehaviour
 	IEnumerator RequestMapState()
 	{
 		//string requestAddress = String.Format("{0}:{0}", url, port);
-		UnityWebRequest request = UnityWebRequest.Get("http://127.0.0.1:4000");
+		UnityWebRequest request = UnityWebRequest.Get("http://127.0.0.1:4000/update");
 		yield return request.Send();
 
 		var playersList = JSON.Parse(request.downloadHandler.text)["players"];
@@ -57,5 +53,55 @@ public class WorkerManager : MonoBehaviour
 			avatar.transform.position = new Vector3(x, 0.5f, y);
 			Debug.Log("Player " + id + " is at position (" + x + ", " + y + ")");
 		}
+	}
+
+	// Wrap initialisation request in coroutine.
+	public void BuildStaticScene()
+	{
+		Debug.Log("Initialising map...");
+		StartCoroutine(GetFixedObjects());
+	}
+
+	// Request map dimensions and static objects, i.e. walls.
+	IEnumerator GetFixedObjects()
+	{
+		Debug.Log("afdsgsdfgdsg");
+		UnityWebRequest request = UnityWebRequest.Get("http://127.0.0.1:4000/start");
+		yield return request.Send();
+
+		var map = JSON.Parse(request.downloadHandler.text);
+		Debug.Log(map);
+
+		// Create plane (floor).
+		float minX = (float) map["minX"].AsInt;
+		float minY = (float) map["minY"].AsInt;
+		float maxX = (float) map["maxX"].AsInt;
+		float maxY = (float) map["maxY"].AsInt;
+
+		GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+		floor.transform.position = new Vector3(minX + maxX, 0.0f, minY + maxY);
+		floor.transform.localScale = new Vector3((maxX - minX) / 10.0f, 1.0f, (maxY - minY) / 10.0f);
+
+		// Create cubes (walls).
+		var objectList = map["objects"];
+
+		for (int i = 0; i < objectList.Count; i++)
+		{
+			var obj = objectList[i];
+
+			string id = "object" + Convert.ToString(obj["id"].AsInt);
+
+			float x = (float) obj["x"].AsInt;
+			float y = (float) obj["y"].AsInt;
+
+			// Create new game object.
+			GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			wall.transform.position = new Vector3(x, 0.5f, y);
+			wall.name = id;
+
+			Debug.Log("Object " + id + " is at position (" + x + ", " + y + ")");
+		}
+
+		// Create spheres (avatars)
 	}
 }
