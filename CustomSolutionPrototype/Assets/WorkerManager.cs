506ef7a34d97f8742aadcc2e021ca5c5424b90f2
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2017-07-07 11:39:12 +0100

    Changed map creation protocol in Unity.

--- CustomSolutionPrototype/Assets/WorkerManager.cs
+++ CustomSolutionPrototype/Assets/WorkerManager.cs
@@ -7,54 +7,19 @@ using SimpleJSON;
 
 public class WorkerManager : MonoBehaviour
 {
-	private const string url = "http://127.0.0.1";
-	private const int port = 4000;
+	private const string reqeustUrl = "http://127.0.0.1:4000";
 
 	// Setup to ping the server every second.
 	void Start()
 	{
 		Debug.Log("Starting the game...");
 
-		BuildStaticScene();
+		MapBuilder mapBuilder = new MapBuilder();
+		mapBuilder.
+
 		InvokeRepeating("UpdateMapState", 0.0f, 1.0f);
 	}
-
-	void BuildStaticScene()
-	{
-		Debug.Log("Initial map.");
-		StartCoroutine(GetFixedObjects());
-	}
-
-	IEnumerator GetFixedObjects()
-	{
-		UnityWebRequest request = UnityWebRequest.Get("http://127.0.0.1:4000");
-		yield return request.Send();
-
-		var objectList = JSON.Parse(request.downloadHandler.text)["objects"];
-		Debug.Log(objectList.Count);
-
-		for (int i = 0; i < objectList.Count; i++)
-		{
-			var obj = objectList[i];
-
-			string id = "object" + Convert.ToString(obj["id"].AsInt);
-			GameObject avatar = GameObject.Find(id);
-
-			float x = (float) obj["x"].AsInt;
-			float y = (float) obj["y"].AsInt;
-
-			if (avatar == null)
-			{
-				// Create new game object.
-				avatar = GameObject.CreatePrimitive(PrimitiveType.Cube);
-				avatar.transform.position = new Vector3(x, 0.5f, y);
-				avatar.name = id;
-			}
-			avatar.transform.position = new Vector3(x, 0.5f, y);
-			Debug.Log("Object " + id + " is at position (" + x + ", " + y + ")");
-		}
-	}
-
+		
 	// Wrap request in a coroutine.
 	void UpdateMapState()
 	{
